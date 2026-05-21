0a2c777addf8b331cfb220aba91100ff837d7afa
LMess131 <alexanderlimaabad@gmail.com>
2026-05-21 17:23:04 -0500

    Correccion de la implementacion de las consultas utilizando diferentes operador

--- MySQL/consultas_realizadas.sql
+++ MySQL/consultas_realizadas.sql
@@ -1,15 +1,16 @@
+
 /*
 CONSULTAS REALIZADAS
 DENTRO DE MI BASE DE DATOS camaronera
 */
 
 /*
--- Consulta 1:
+-- Consulta 1: OPERADOR: Reunión Natural (JOIN) + Proyección (π)
 ¿Qué encargado administra cada piscina y cuántos camarones
 fueron sembrados inicialmente en cada lote?
 
 π nombres, apellidos, ubicacion, cantidad_inicial
-(σ cedula==id_encargado (encargado * piscina) ⋈ lote_animal)
+(Encargado ⋈ Piscina ⋈ Lote_Animal)
 */
 SELECT e.nombres,
        e.apellidos,
@@ -21,13 +22,14 @@ ON e.cedula = p.id_encargado
 INNER JOIN Lote_Animal l
 ON p.id_piscina = l.id_piscina;
 
+
 /*
--- Consulta 2:
-¿Qué lote recibió alimento tipo Engorde
+-- Consulta 2: OPERADOR: Selección (σ) + Proyección (π)
+¿Qué lotes recibieron alimento tipo "Engorde"
 y en qué horario fue suministrado?
 
 π id_lote, tipo_alimento, hora
-(σ tipo_alimento=='Engorde' (alimentacion ⋈ horario ⋈ lote_animal))
+(σ tipo_alimento='Engorde' (Alimentacion ⋈ Horario))
 */
 SELECT a.id_lote,
        a.tipo_alimento,
@@ -35,77 +37,65 @@ SELECT a.id_lote,
 FROM Alimentacion a
 INNER JOIN Horario h
 ON a.id_horario = h.id_horario
-INNER JOIN Lote_Animal l
-ON a.id_lote = l.id_lote
 WHERE a.tipo_alimento = 'Engorde';
 
-/*
--- Consulta 3:
-¿Qué enfermedades fueron detectadas en los lotes
-y qué encargado es responsable de la piscina?
 
-π nombres, apellidos, id_lote, nombre
-(σ cedula==id_encargado (encargado * piscina)
-⋈ lote_animal ⋈ control_sanitario ⋈ enfermedad)
+/*
+-- Consulta 3: OPERADOR: Intersección (∩)
+¿Qué lotes tienen tanto registro de alimentación
+como registro de control sanitario?
+(es decir, lotes que aparecen en AMBAS tablas)
+
+π id_lote (Alimentacion)
+∩
+π id_lote (Control_Sanitario)
 */
-SELECT e.nombres,
-       e.apellidos,
-       l.id_lote,
-       en.nombre AS enfermedad
-FROM Encargado e
-INNER JOIN Piscina p
-ON e.cedula = p.id_encargado
+SELECT a.id_lote
+FROM Alimentacion a
 INNER JOIN Lote_Animal l
-ON p.id_piscina = l.id_piscina
-INNER JOIN Control_Sanitario cs
-ON l.id_lote = cs.id_lote
-INNER JOIN Enfermedad en
-ON cs.id_enfermedad = en.id_enfermedad;
+ON a.id_lote = l.id_lote
 
-/*
--- Consulta 4:
-¿Qué piscinas ubicadas en Rumeyaca
-tienen lotes registrados?
+INTERSECT
 
-π id_piscina, ubicacion, id_lote
-(σ ubicacion=='Rumeyaca' (piscina ⋈ lote_animal))
-*/
-SELECT p.id_piscina,
-       p.ubicacion,
-       l.id_lote
-FROM Piscina p
+SELECT cs.id_lote
+FROM Control_Sanitario cs
 INNER JOIN Lote_Animal l
-ON p.id_piscina = l.id_piscina
-WHERE p.ubicacion = 'Rumeya';
+ON cs.id_lote = l.id_lote;
+
 
 /*
--- Consulta 5:
-¿Qué tipo de alimento y cuántos kilogramos
-se suministraron en cada lote?
+-- Consulta 4: OPERADOR: Diferencia (−)
+¿Qué lotes NO tienen ningún registro
+de control de crecimiento?
 
-π id_lote, cantidad_inicial, tipo_alimento, cantidad
-(lote_animal ⋈ alimentacion)
+π id_lote (Lote_Animal)
+−
+π id_lote (Control_Crecimiento)
 */
-SELECT l.id_lote,
-       l.cantidad_inicial,
-       a.tipo_alimento,
-       a.cantidad
+SELECT l.id_lote
 FROM Lote_Animal l
-INNER JOIN Alimentacion a
-ON l.id_lote = a.id_lote;
+
+EXCEPT
+
+SELECT c.id_lote
+FROM Control_Crecimiento c;
+
 
 /*
--- Consulta 6:
-¿Cuál es el peso promedio registrado
-en cada control de crecimiento por lote?
+-- Consulta 5: OPERADOR: Unión (∪)
+¿Qué lotes tienen registro de alimentación
+o registro de control de crecimiento (o ambos)?
 
-π id_lote, fecha, peso_promedio
-(lote_animal ⋈ control_crecimiento)
+π id_lote (Alimentacion)
+∪
+π id_lote (Control_Crecimiento)
 */
-SELECT l.id_lote,
-       c.fecha,
-       c.peso_promedio
-FROM Lote_Animal l
-INNER JOIN Control_Crecimiento c
-ON l.id_lote = c.id_lote;
+SELECT a.id_lote
+FROM Alimentacion a
+
+UNION
+
+SELECT c.id_lote
+FROM Control_Crecimiento c;
+
 
